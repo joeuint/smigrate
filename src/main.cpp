46915fb59c76872ac622de6164a9639ce0f5d98d
Joseph Umana <joseph@josephumana.com>
2026-01-14 10:41:00 -0500

    subcmd: new
    Added the new subcmd to create migrations with the correct filename. It
    read the existing migrations to find the correct ID. Still some work to
    do before finalizing the feature such as better exception handling when
    no migration dir is found.
    
    Signed-off-by: Joseph Umana <joseph@josephumana.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <cctype>
 #include <memory>
+#include <fstream>
 #include <print>
 #include <cassert>
 #include <string>
@@ -7,56 +9,6 @@
 #include <unordered_map>
 
 constexpr std::string_view filenameRegex { "^([0-9]{6})_([a-zA-Z_]+)\\.(up)\\.sql" };
-
-class SubCommandHandler {
-public:
-    virtual void execute() = 0;
-    virtual ~SubCommandHandler() = default;
-};
-
-class NewHandler : public SubCommandHandler {
-    void execute() override {
-        std::println("pong");
-    }
-};
-
-class SubCommandParser {
-    const int m_argc;
-    std::vector<std::string> m_argv;
-    std::unordered_map<std::string, std::unique_ptr<SubCommandHandler>> m_cmds;
-
-    void addCmds() {
-        m_cmds.emplace("new", std::make_unique<NewHandler>());
-    }
-
-public:
-
-    SubCommandParser(int argc, char* argv[]) : m_argc(argc) {
-        m_argv.reserve(argc);
-
-        for (int i = 0; i < argc; i++) {
-            m_argv.push_back(argv[i]);
-        }
-    }
-
-    void runCmds() {
-        addCmds();
-
-        if (m_argc < 2) {
-            std::println("Not enough args");
-            return;
-        }
-
-        auto it = m_cmds.find(m_argv[1]);
-
-        if (it == m_cmds.end()) {
-            std::println("subcmd not found");
-            return;
-        }
-
-        it->second->execute();
-    };
-};
 
 class MigrationNode {
     public:
@@ -86,14 +38,15 @@ class MigrationNode {
 };
 
 class MigrationList {
+public:
     MigrationNode* m_head { nullptr }; // last node
     MigrationNode* m_tail { nullptr }; // first node
     
-    public:
-        ~MigrationList();
-        void push(const int id, const std::string_view name, const std::string_view upFile, const std::string_view downFile);
-        void loadMigrations(const std::string_view path = "./migrations");
-        void print();
+public:
+    ~MigrationList();
+    void push(const int id, const std::string_view name, const std::string_view upFile, const std::string_view downFile);
+    void loadMigrations(const std::string_view path = "./migrations");
+    void print();
 };
 
 void MigrationList::push(const int id, const std::string_view name, const std::string_view upFile, const std::string_view downFile) {
@@ -169,15 +122,118 @@ void MigrationList::loadMigrations(const std::string_view path) {
         push(std::stoi(matches[1].str()), matches[2].str(), path, downFilePath);
     }
 }
+// TODO: It's all a mess. break it into seperate files.
+class SubCommandHandler {
+public:
+    virtual void execute(int argc, const std::vector<std::string>& argv, MigrationList& migs) = 0;
+    virtual ~SubCommandHandler() = default;
+};
+
+class NewHandler : public SubCommandHandler {
+    int getNewNodeId(MigrationNode* tail) {
+        int newId = 0;
+        
+        while (tail != nullptr) {
+            newId = std::max(newId, tail->getId() + 1);
+
+            tail = tail->next;
+        }
+
+        return newId;
+    }
+
+    bool verifyCharset(const std::string& name) {
+        return std::all_of(
+            name.begin(),
+            name.end(),
+            [](char c) {
+                return std::isalpha(c) || c == '_';
+            }
+        );
+
+    }
+
+    void execute(int argc, const std::vector<std::string>& argv, MigrationList& migs) override {
+
+        if (argc < 3) {
+            std::println("Not enough arguments");
+            return;
+        }
+
+        if (!verifyCharset(argv[2])) {
+            std::println("Only letters and underscores are allowed in names!");
+            return;
+        }
+                
+
+        migs.loadMigrations();
+
+
+        int newId = getNewNodeId(migs.m_tail);
+
+        std::println("{}", newId);
+
+        std::string format = std::format("./migrations/{:06}_{}", newId, argv[2]);
+
+        std::string upFile = format + ".up.sql";
+        std::string downFile = format + ".down.sql";
+
+        // TODO: Validate via the regex just to be safe
+
+        std::println("Created: {}", upFile);
+        std::println("Created: {}", downFile);
+        
+        (void)std::ofstream { upFile };
+        (void)std::ofstream { downFile };
+    }
+};
+
+class SubCommandParser {
+    const int m_argc;
+    std::vector<std::string> m_argv;
+    
+    MigrationList& m_migs;
+
+    std::unordered_map<std::string, std::unique_ptr<SubCommandHandler>> m_cmds;
+
+    void addCmds() {
+        m_cmds.emplace("new", std::make_unique<NewHandler>());
+    }
+
+public:
+
+    SubCommandParser(int argc, char* argv[], MigrationList& migs) : m_argc(argc), m_migs(migs) {
+        m_argv.reserve(argc);
+
+        for (int i = 0; i < argc; i++) {
+            m_argv.push_back(argv[i]);
+        }
+    }
+
+    void runCmds() {
+        addCmds();
+
+        if (m_argc < 2) {
+            std::println("Not enough arguments");
+            return;
+        }
+
+        auto it = m_cmds.find(m_argv[1]);
+
+        if (it == m_cmds.end()) {
+            std::println("subcmd not found");
+            return;
+        }
+
+        it->second->execute(m_argc, m_argv, m_migs);
+    };
+};
+
 
 int main(int argc, char* argv[]) {
     MigrationList migs;
 
-    SubCommandParser subCmd { argc, argv };
+    SubCommandParser subCmd { argc, argv, migs };
 
     subCmd.runCmds();
-
-    migs.loadMigrations();
-
-    migs.print();
 }
